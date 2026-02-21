06460c272c5404290b03f8f0b994d966a0d2b9dd
joaovictorjs <skai7ami@gmail.com>
2026-02-21 14:15:04 -0300

    chore: create category table

---
+++ src/Atlas.Infrastructure/Data/EntityTypeConfigurations/CategoryEntityTypeConfiguration.cs
@@ -0,0 +1,24 @@
+using Atlas.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Atlas.Infrastructure.Data.EntityTypeConfigurations
+{
+    internal class CategoryEntityTypeConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("categories");
+            builder.HasKey(category => category.Id);
+            builder.Property(category => category.Id).HasColumnName("id");
+            builder
+                .Property(category => category.Name)
+                .IsRequired()
+                .HasMaxLength(Category.NameMaxLength)
+                .HasColumnName("name");
+            builder.Property(category => category.CreatedAt).IsRequired().HasColumnName("created_at");
+            builder.Property(category => category.UpdatedAt).IsRequired().HasColumnName("updated_at");
+
+        }
+    }
+}
---
+++ src/Atlas.Infrastructure/Data/Migrations/20260221171442_CreateCategoryTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Atlas.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "categories");
+        }
+    }
+}
